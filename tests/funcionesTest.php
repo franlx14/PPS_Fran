cb0afd5d4ddea1980a7f36c29156f50e697ddd6a
franlx14 <frapozpra@alu.edu.gva.es>
2024-05-22 17:28:26 +0200

    Update funcionesTest.php

--- tests/funcionesTest.php
+++ tests/funcionesTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/home/runner/work/PPS_Fran/PPS_Fran/html/funciones.php';
+require_once '/home/runner/work/PPS_Fran/html/funciones.php';
 use PHPUnit\Framework\TestCase;
 
 class funcionesTest extends TestCase
