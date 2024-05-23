7c1230905903c3b6a2fa64e85282b7c9e9d6e28a
franlx14 <frapozpra@alu.edu.gva.es>
2024-05-23 23:07:29 +0200

    Update conexion_bd.php

--- html/conexion_bd.php
+++ html/conexion_bd.php
@@ -12,10 +12,4 @@ $db_password = getenv('DB_PASSWORD');
 
 // Create connection
 $conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 ?>
