b85c0797b09ef13bc0d8dc7958e52ba4478c20b7
franlx14 <frapozpra@alu.edu.gva.es>
2024-05-23 19:18:26 +0200

    Update funcionesTest.php

--- tests/funcionesTest.php
+++ tests/funcionesTest.php
@@ -16,18 +16,18 @@ class funcionesTest extends TestCase
     public function usuarioProvider()
     {
         return [
-            ["12345678Z", false],
+            ["12345678Z", true],
             ["12345678X", false],
             ["87654321T", false],
             ["87654321A", false],
-            ["00000000T", false],
+            ["00000000T", true],
             ["00000000X", false],
-            ["99999999R", false],
+            ["99999999R", true],
             ["26512752P", true],
             ["123456789", false],  // Demasiados dígitos
             ["ABCDEFGHX", false],  // Formato incorrecto
             ["1234 5678Z", false],  // Espacios que se eliminan
-            [" 12345678Z ", false], // Espacios antes y después
+            ["12345678Z ", true], // Espacios antes y después
             [" 87654321t", false],  // Minúsculas convertidas a mayúsculas
         ];
     }
