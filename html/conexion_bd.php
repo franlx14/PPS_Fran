774db83331e17c5559a1e44e67c63058ffe3457c
franlx14 <frapozpra@alu.edu.gva.es>
2024-05-24 00:20:51 +0200

    Update conexion_bd.php

--- html/conexion_bd.php
+++ html/conexion_bd.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 // Retrieve database credentials from environment variables
